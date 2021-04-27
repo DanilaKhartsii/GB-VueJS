8f40d5931993147b94275048a5d877e0e38ced4e
dharciy <danila.khartsiy@s-telematics.ru>
2021-04-27 18:05:25 +0300

    homework #1 add 2 methods for calculate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,63 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input v-model.number="operand1"/>
+    <input v-model.number="operand2"/>
+    = {{ result }}
+    <div class="buttons">
+      <button @click="calculate('+')">+</button>
+      <button @click="calculate('-')">-</button>
+      <button @click="calculate('/')">/</button>
+      <button @click="calculate('*')">*</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0
+    }
+  },
+  methods: {
+    // calculate2 (arg) {
+    //   switch (arg) {
+    //     case 'sum':
+    //       this.result = this.operand1 + this.operand2
+    //       break
+    //     case 'sub':
+    //       this.result = this.operand1 - this.operand2
+    //       break
+    //     case 'div':
+    //       this.result = this.operand1 / this.operand2
+    //       break
+    //     case 'mul':
+    //       this.result = this.operand1 * this.operand2
+    //       break
+    //     default:
+    //       alert('Где то ошибка')
+    //       break
+    //   }
+    // },
+    calculate (arg) {
+      /* eslint-disable-next-line */
+      this.result = eval(`${this.operand1} ${arg} ${this.operand2}`)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.hello {
+  .buttons {
+    margin-top: 10px;
+    height: 20px;
+    button {
+      margin-right: 10px;
+    }
+  }
 }
 </style>
